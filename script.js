af8d4084084efe75960b6608ef74e5c07d164784
Py World <163068584+bg1qr@users.noreply.github.com>
2024-03-21 06:14:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const toolbut = document.getElementById("toolbut");
+const toolbar2 = document.getElementById("toolbar2");
+const search =document.getElementById("search")
+function toolButt() {
+				toolbar2.style.display="block";
+}
+function toolHed() {
+				toolbar2.style.display="none";
+}
+//function searchButt() {
+//				console.log(search.value)
+//}
+
+function searchButt(){
+    let textToSearch = document.getElementById("search").value;
+    let paragraph = document.getElementById("paragraph");
+    textToSearch = textToSearch.replace(/[.*+?^${}()|[\]\\]/g,"\\$&");
+
+    let pattern = new RegExp(`${textToSearch}`,"gi");
+
+    paragraph.innerHTML = paragraph.textContent.replace(pattern, match => `<mark>${match}</mark>`)
+}
